8e24f537bfa6ea95acbac5f00f1177b0fad50162
Taniakays <ms.tkatiyo@gmail.com>
2022-10-15 12:47:32 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers of base starting 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
