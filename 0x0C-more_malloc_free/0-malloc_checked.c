6c0f68bc39fdeb3be8b22148fb97808d5363ffd3
Taniakays <ms.tkatiyo@gmail.com>
2022-11-10 08:27:43 -0800

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function should cause normal process termination
+ * @b: allocated memory
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *y;
+
+	y = malloc(b);
+	if (y == NULL)
+		exit(98);
+	return (i);
+}
