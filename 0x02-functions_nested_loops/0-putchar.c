69f99f440eb9ded2d7e4bd7bd49315eb6c22a387
Taniakays <ms.tkatiyo@gmail.com>
2024-02-18 04:54:19 -0800

    main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
-
 /**
- * main - Prints "_putchar" followed by a new line
- *
- * Return: Always 0.
+ * main - program that print
+ * Return: Always 0
  */
+
 int main(void)
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -16,6 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
 	return (0);
+
 }
